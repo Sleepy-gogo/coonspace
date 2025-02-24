76ef115dda56e85173a8ffde6d245f4fee296dc3
Axel Castaño <axelcastano@proton.me>
2025-02-24 07:01:46 -0300

    Improved background look

--- src/components/background/bottom-gradient.tsx
+++ src/components/background/bottom-gradient.tsx
@@ -1,6 +1,6 @@
 function BottomGradient() {
   return (
-    <div className="fixed inset-1 -z-[5] bg-gradient-to-t from-blue-500 opacity-[0.07] to-transparent" />
+    <div className="fixed inset-1 -z-[5] bg-gradient-to-t from-blue-600 to-transparent opacity-[0.15]" />
   );
 }
 
--- src/components/background/stars.tsx
+++ src/components/background/stars.tsx
@@ -28,7 +28,7 @@ function Stars({
 
   return (
     <div
-      className="pointer-events-none absolute inset-0 -z-10"
+      className="pointer-events-none fixed inset-0 -z-10"
       style={{ opacity }}
     >
       {stars.map((star, index) => (
